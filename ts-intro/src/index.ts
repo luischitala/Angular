b2036f1559b862ec9f7bd0c3e2a6f220d54214b7
unknown <lrchitala@gmail.com>
2021-04-25 21:54:42 -0500

    initial commit

---
+++ ts-intro/src/index.ts
@@ -0,0 +1,13 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Strider';
+// Poner 2 posibles tipos de datos
+let hp: number | string = 95;
+let estaVivo: boolean = false;
+
+hp = 'FULL';
+nombre = 'Fernando';
+
+console.log(nombre, hp);
